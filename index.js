9b471422d63bdaa9a59f2f591a963cf122638b6c
achallion <bhavyarocks19@gmail.com>
2024-03-13 23:42:53 +0530

    added root router
    Signed-off-by: achallion <bhavyarocks19@gmail.com>

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const https = require('https');
 const path = require('path');
 const fs = require('fs');
+const root_router = require('./routes/root_router')
 
 let httpsOptions = {
 	key: fs.readFileSync(path.join(__dirname, "privatekey.pem")),
@@ -13,10 +14,7 @@ const server = express();
 const HTTP_PORT = process.env.HTTP_PORT | 3001;
 const HTTPS_PORT = process.env.HTTPS_PORT | 4001;
 
-server.use((req, res) => {
-	res.send("ger")
-});
-
+server.use("/", root_router);
 
 https.createServer(httpsOptions, server).listen(HTTPS_PORT, () => console.log(`HTTPS Server running on port ${HTTPS_PORT}`));
 server.listen(HTTP_PORT, () => console.log(`HTTP Server running on port ${HTTP_PORT}`))
---
+++ routes/root_router.js
@@ -0,0 +1,5 @@
+const express = require('express')
+
+const root_router = express.Router();
+
+module.exports = root_router;
