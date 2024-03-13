2dc0b329858591f96cede7a2972d96ac7d96be73
achallion <bhavyarocks19@gmail.com>
2024-03-13 23:58:12 +0530

    logging
    Signed-off-by: achallion <bhavyarocks19@gmail.com>

--- routes/root_router.js
+++ routes/root_router.js
@@ -1,5 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
+const view_router = require('./view_router')
 
 const root_router = express.Router();
+
+root_router.use(morgan('tiny'));
+
+// root_router.use("/view", view_router)
 
 module.exports = root_router;
---
+++ routes/view_router.js
@@ -0,0 +1,11 @@
+// const express = require('express')
+// const path = require('path')
+
+// view_router = express.Router();
+
+// view_router.set('views',path.join(__dirname, 'views'));
+// view_router.set('view engine',"ejs")
+
+// view_router.get();
+
+// module.exports = view_router;
